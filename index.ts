96ffe22874cf002369275f665c899798f8f4875d
Marce1in <marcelo.desenvolvedor.contato@gmail.com>
2024-09-03 11:56:22 -0300

    Mudando imprimir para listar, em index.ts

--- index.ts
+++ index.ts
@@ -12,7 +12,7 @@ while(true){
     console.log("2 - Frear");
     console.log("3 - Subir marcha");
     console.log("4 - Descer marcha");
-    console.log("5 - Imprimir dados do veículo");
+    console.log("5 - Listar dados do veículo");
     console.log("0 - Sair");
 
     const opcao = +teclado('Escolha uma opção: ');
